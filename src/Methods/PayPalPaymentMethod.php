2673757f94a08dcdaa5ad3a66fe670d32f42b6df
Drew Rowan <andrew.rowan@wallee.com>
2022-05-12 15:27:55 +0200

    WAL-66794 changing image link

--- src/Methods/PayPalPaymentMethod.php
+++ src/Methods/PayPalPaymentMethod.php
@@ -58,7 +58,7 @@ class PayPalPaymentMethod extends AbstractPaymentMethod
      */
     public function getIcon(): string
     {
-        return "https://app-wallee.com/en-US/s/2415/resource/web/image/pp2.svg3";
+        return $this->getImagePath('pp2.svg');
     }
 
     /**
