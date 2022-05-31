06ceb46443b1d9b9bf9b97fb9a6134d7eba999c1
Drew Rowan <andrew.rowan@wallee.com>
2022-05-31 17:10:35 +0200

    added php version log

--- src/Methods/CreditDebitCardPaymentMethod.php
+++ src/Methods/CreditDebitCardPaymentMethod.php
@@ -55,6 +55,7 @@ class CreditDebitCardPaymentMethod extends AbstractPaymentMethod
      */
     public function getIcon(): string
     {
+        $this->getLogger(__METHOD__)->info('phpoversion', PHP_VERSION);
         return "https://app-wallee.com/en-US/s/2415/resource/web/image/pp2.svg?strategy=snapshot&snapshot=53853";
     }
 
